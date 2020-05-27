3f7a9eba6d84fd6f118bc2e6d122f75dc3c1df11
unholst <unholst@users.noreply.github.com>
2020-05-27 15:28:37 +0200

    cnf in Main eingebunden

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import eingabe
 import cyk
 import tabular
+import cnf
 
 
 
+word = eingabe.new_word()
 grammar = eingabe.cfg()
 eingabe.cfg.new_grammar(grammar)
-word = eingabe.new_word()
-#hier wird cnf aufgerufen
+
+grammar = cnf.epsilon_elim(grammar)             #hier wird cnf aufgerufen
 table = cyk.cyk(word)
 
 tableau = tabular.to_latex(table,len(word))
