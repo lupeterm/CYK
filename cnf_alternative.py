0600fa8885e1508449b3d82ebd072c1331592448
lupeterm <lukas.petermann+1@st.ovgu.de>
2020-09-11 11:21:19 +0200

    Revert "cleanup and improved variable naming"
    This reverts commit 96333e3716a889ea9bc53d436b9269fcb1239541.

--- cnf_alternative.py
+++ cnf_alternative.py
@@ -6,31 +6,38 @@ def non_iso_term_elim_alternative(rules, variables, alphabet):
     """alternative version of elimination of non isolated terminal symbols"""
     alph = set(string.ascii_uppercase) - set(variables)
     length_difference = len(alphabet) - len(alph)
-    needed_symbols = [set(string.ascii_uppercase).pop() for _ in range(int(length_difference / 10) + 1)]
+    alternate_alph = set(string.ascii_uppercase)
+    needed_symbols = [alternate_alph.pop() for _ in range(int(length_difference / 10) + 1)]
     new_dict = dict()
     for symbol in needed_symbols:
         for diff in range(length_difference):
             alph.add(symbol + str(diff))
     map_term_not_term = [(char, symbol) for char, symbol
                          in zip(alphabet, alph)]
-    for keys, sets_of_strings in rules.items():
-        string_set = sets_of_strings.copy()
-        for strings in string_set:
-            string_update = strings
+    for keys, values in rules.items():
+        new_dict[keys] = set()
+        tmp_val = values.copy()
+        for val in tmp_val:
+            tmp_str = val
             for term_symbol in map_term_not_term:
-                if term_symbol[0] in string_update and len(string_update) > 1:
-                    string_update = string_update.replace(term_symbol[0], term_symbol[1])
+                if term_symbol[0] in tmp_str and len(tmp_str) > 1:
+                    tmp_str = tmp_str.replace(term_symbol[0], term_symbol[1])
                     new_dict[term_symbol[1]] = set(term_symbol[0])
-            string_set.remove(strings)
-            string_set.add(string_update)
+            tmp_val.remove(val)
+            tmp_val.add(tmp_str)
 
-        new_dict[keys] = string_set
-    for set_of_strings in new_dict.values():  # TODO: make it work without iterating twice
-        for string_set in set_of_strings:
-            if len(string_set) > 1:
+        new_dict[keys].update(tmp_val)
+    repeat = False
+    for value in new_dict.values():  # make sure it worked, reiterate if needed
+        for strings in value:
+            if len(strings) > 1:
                 for term in alphabet:
-                    if term in string_set:
-                        return non_iso_term_elim_alternative(new_dict, new_dict.keys(), alphabet)
+                    if term in strings:
+                        repeat = True
+    if repeat:
+        return non_iso_term_elim_alternative(new_dict,
+                                             (key for key, values in new_dict.items()),
+                                             alphabet)
     return new_dict, True
 
 
@@ -40,24 +47,24 @@ def long_right_alternative(rules):
     alph = set(string.ascii_uppercase) - set(alternate_keys)
     new_dict = dict()
     am_new_keys = 0
-    new_key = alph.pop() + str(am_new_keys)
-    for key, sets_of_strings in rules.items():
-        string_set = sets_of_strings.copy()
-        new_dict[key] = string_set
-        for old_value in sets_of_strings:
-            amount_integers = len([num for num in [char for char in old_value]
+    new_key = alph.pop()+str(am_new_keys)
+    for key, values in rules.items():
+        tmp_val = values.copy()
+        new_dict[key] = tmp_val
+        for strings in values:
+            amount_integers = len([num for num in [char for char in strings]
                                    if num not in string.ascii_uppercase])
-            if len(old_value) - amount_integers > 2:
-                updated_value = old_value
+            if len(strings) - amount_integers > 2:
+                tmp_str = strings
                 count = 0
-                new_value = ""
-                for char in reversed(updated_value):
+                new_val = ""
+                for char in reversed(tmp_str):
                     if count == 2:
                         break
                     if char not in string.ascii_uppercase:
-                        new_value = char + new_value
+                        new_val = char + new_val
                         continue
-                    new_value = char + new_value
+                    new_val = char + new_val
                     count += 1
                 am_new_keys += 1
                 if am_new_keys > 9:
@@ -65,15 +72,19 @@ def long_right_alternative(rules):
                     am_new_keys = 0
                 if len(new_key) > 1:
                     new_key = ''.join(list(new_key)[:-1]) + str(am_new_keys)
-                updated_value = updated_value[:-len(new_value)] + new_key
-                new_dict[key].remove(old_value)
-                new_dict[key].add(updated_value)
-                new_dict[new_key] = set(new_value)
-    for key, sets_of_strings in new_dict.items():
-        for strings in sets_of_strings:
+                tmp_str = tmp_str[:-len(new_val)] + new_key
+                new_dict[key].remove(strings)
+                new_dict[key].add(tmp_str)
+                new_dict[new_key] = set()
+                new_dict[new_key].add(new_val)
+    repeat = False
+    for key, values in new_dict.items():
+        for strings in values:
             amount_integers = len([num for num in
                                    [char for char in strings]
                                    if num not in string.ascii_uppercase])
             if len(strings) - amount_integers > 2:
-                return long_right_alternative(new_dict)
+                repeat = True
+    if repeat:
+        return long_right_alternative(new_dict)
     return new_dict
