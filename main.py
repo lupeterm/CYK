b180c9a7a7f9d77ef74ae6d5a7eeeea146967b54
lupeterm <lukas.petermann+1@st.ovgu.de>
2020-09-18 14:58:39 +0200

    modified latex output (wip)

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ grammar = eingabe.CFG()
 # eingabe.cfg.new_grammar(grammar)
 word = eingabe.new_word()
 cnf_test.print_grammar(grammar.rules)
-grammar.rules = {
-    'S': {'aACa'},
-    'A': {'B', 'a'},
-    'B': {'C', 'c'},
-    'C': {'cC', r'\E'}
-}
+grammar.rules = dict(S={'aACa'}, A={'B', 'a'}, B={'C', 'c'}, C={'cC', r'\E'})
 grammar.start = 'S'
 grammar.alphabet = {'a', 'b', 'c'}
 grammar.variables = set(key for key in grammar.rules)
@@ -22,7 +17,8 @@ grammar.rules = cnf.cnf(grammar)  # bring CFG in Chomsky NF
 print("cnf done")
 cnf_test.print_grammar(grammar.rules)
 table = cyk.cyk(grammar, word)  # run CYK algorithm
-tableau = tabular.to_latex(table, word, grammar.start)
+tableau = tabular.to_latex(table, word, grammar.start, grammar.rules)
 file = open(file="CYK_Tableau.tex", mode="w")
 file.write(tableau)
+file.close()
 print("\nwritten in CYK_Tableau.tex")
--- tabular.py
+++ tabular.py
@@ -3,7 +3,7 @@ from string import Template
 import tabulate
 
 
-def to_latex(table, word, start):
+def to_latex(table, word, start, rules):
     """still formatting"""
 
     # custom indices for table
@@ -19,10 +19,10 @@ def to_latex(table, word, start):
 
     # conclusion and insertion of cyk table
     if table[0][-1]:
-        is_in = r'$w \in L$' if start in table[0][-1][0] else r'$w \notin L$'
+        is_in = '$$w \\in L \n' if start in table[0][-1][0] else '$$w \\notin L \n'
     else:
-        is_in = r'$w \notin L$'
-    latex_string = Template(template).safe_substitute(table=latex_string, word=is_in)
+        is_in = '$$w \\notin L \n'
+    latex_string = Template(template).safe_substitute(table=latex_string, word=is_in,)
 
     # center columns
     latex_string = latex_string.replace(r'\begin{tabular}'r'{r' + ("l" * len(word)),
@@ -34,5 +34,14 @@ def to_latex(table, word, start):
     latex_string = latex_string.replace("', '", ", ")
     latex_string = latex_string.replace("1 & ", "\\hline\n 1 & ", 1)
     latex_string = latex_string.replace("[]", r'$\emptyset$')
-
+    latex_string = Template(latex_string).safe_substitute(array=grammar_to_latex(rules))
     return latex_string
+
+
+def grammar_to_latex(rules):
+    table_string = ''
+    for key, values in rules.items():
+        table_string = table_string + f'{key} & \\rightarrow & \\{values} & \n'
+    table_string = table_string.replace("'", "")
+    table_string = table_string.replace('}', '\\}')
+    return table_string
